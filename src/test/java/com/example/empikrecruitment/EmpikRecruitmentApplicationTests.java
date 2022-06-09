30d4a4bf28f31f582f71ce3c43ebf54d5452ad80
jakubseta <jakub.seta@nubisoft.pl>
2022-06-09 13:32:24 +0200

    Initial commit

---
+++ src/test/java/com/example/empikrecruitment/EmpikRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empikrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
